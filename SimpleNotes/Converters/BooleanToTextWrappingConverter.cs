c63c9fe213fcb0536f19b051683c37110952cfc6
David Dostal <david.dostal1112@gmail.com>
2018-07-02 16:53:35 +0200

    Implement binding to textwrapping menuitem

---
+++ SimpleNotes/Converters/BooleanToTextWrappingConverter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace SimpleNotes.Converters
+{
+    [ValueConversion(typeof(bool), typeof(TextWrapping))]
+    public class BooleanToTextWrappingConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+            => ((bool)value) ? TextWrapping.Wrap : TextWrapping.NoWrap;
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+            => ((TextWrapping)value) == TextWrapping.NoWrap ? false : true;
+    }
+}
